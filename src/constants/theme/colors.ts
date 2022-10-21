66c8ad11e698aec15708587dcc7764beba27996d
Jean Wylmer <jean_wylmer@hotmail.com>
2022-10-20 21:13:08 -0300

    feat: constants

---
+++ src/constants/theme/colors.ts
@@ -0,0 +1,12 @@
+const colors = {
+  background_gray: '#E9E9E9',
+  background_white: '#fdfffc',
+  primary: '#2ec4b6',
+  alert: '#ff9f1c',
+  danger: '#db3a34',
+  success: '#8ac926',
+  gray: '#C8C7C7',
+  white: '#FDFDFD',
+}
+
+export default colors;
